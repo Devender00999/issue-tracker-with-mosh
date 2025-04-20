69799f86b9414f9c013e010a4728446395b48d11
Devender Kumar <Devender00999@gmail.com>
2025-04-20 21:56:20 +0530

    Added latest issue card

---
+++ app/components/LatestIssues.tsx
@@ -0,0 +1,48 @@
+import { prisma } from "@/prisma/client";
+import { Avatar, Card, Flex, Heading, Table } from "@radix-ui/themes";
+import React from "react";
+import IssueBadge from "./IssueBadge";
+import Link from "next/link";
+
+const LatestIssues = async () => {
+   const issues = await prisma.issue.findMany({
+      take: 5,
+      include: { assignedToUser: true },
+   });
+   return (
+      <Card>
+         <Heading className="px-2 mb-2">Latest issues</Heading>
+         <Table.Root>
+            <Table.Body>
+               {issues.map((issue) => (
+                  <Table.Row
+                     className="last:border-0 last:border-hidden"
+                     key={issue.id}
+                  >
+                     <Table.Cell>
+                        <Flex justify="between" align="center">
+                           <Flex direction="column" align="start" gap="2">
+                              <Link href={`/issues/${issue.id}`}>
+                                 {issue.title}
+                              </Link>
+                              <IssueBadge status={issue.status} />
+                           </Flex>
+                           {issue.assignedToUser && (
+                              <Avatar
+                                 size="2"
+                                 fallback="?"
+                                 src={issue.assignedToUser?.image!}
+                                 radius="full"
+                              />
+                           )}
+                        </Flex>
+                     </Table.Cell>
+                  </Table.Row>
+               ))}
+            </Table.Body>
+         </Table.Root>
+      </Card>
+   );
+};
+
+export default LatestIssues;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import LatestIssues from "./components/LatestIssues";
 import Pagination from "./components/Pagination";
 
 export default function Home() {
    return (
       <div>
-         <Pagination currentPage={10} itemCounts={100} pageSize={10} />
+         <LatestIssues />
       </div>
    );
 }
